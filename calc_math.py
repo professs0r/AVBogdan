9e84d7bb10b6133c084c0a7bde93c0d7a6d1fa9d
professs0r <dok95651@gmail.com>
2022-02-06 12:16:53 +0300

    06.02.22

--- calc_math.py
+++ calc_math.py
@@ -4,19 +4,99 @@ import numpy
 from numpy import linalg
 import math
 import networkx
-
+import plotly
+import pandas
+import plotly.express as px
+import plotly.graph_objs as go
+import
 
 # global variables
 
-COUNT_NODES = 10
+COUNT_NODES = 9
 COUNT_BRANCHES = 17
 
-# инициализация графа
-def func_initializetion(count_nodes, count_branches):
-    graph = 
+# initialization nondirected graph
+def func_initialization_nondirected_adjacency_list(count_nodes, count_branches):
+    graph = networkx.Graph()
+    for nodes in range(count_nodes):
+        graph.add_node(nodes)
+    nondirected_adjacency_list= numpy.array([(1,3,5,7,9),
+                                             (0,2,4),
+                                             (1,3,5),
+                                             (0,2,6),
+                                             (1,5,7),
+                                             (0,2,4,6,8),
+                                             (3,5,9),
+                                             (0,4,8),
+                                             (5,7,9),
+                                             (0,6,8)])
+    for start in range(len(nondirected_adjacency_list)):
+        for stop in range(len(nondirected_adjacency_list[start])):
+            graph.add_edge(start, nondirected_adjacency_list[start][stop])
+    return graph
+def func_initialization_nondirected_adjacency_matrix(count_nodes, count_branches):
+    graph = networkx.Graph()
+    for nodes in range(count_nodes):
+        graph.add_node(nodes)
+    nondirected_adjacency_matrix = numpy.array([(0,1,0,1,0,1,0,1,0,1),
+                                                (1,0,1,0,1,0,0,0,0,0),
+                                                (0,1,0,1,0,1,0,0,0,0),
+                                                (1,0,1,0,0,0,1,0,0,0),
+                                                (0,1,0,0,0,1,0,1,0,0),
+                                                (1,0,1,0,1,0,1,0,1,0),
+                                                (0,0,0,1,0,1,0,0,0,1),
+                                                (1,0,0,0,1,0,0,0,1,0),
+                                                (0,0,0,0,0,1,0,1,0,1),
+                                                (1,0,0,0,0,0,1,0,1,0)])
+    for start in range(len(nondirected_adjacency_matrix)):
+        for stop in range(len(nondirected_adjacency_matrix[start])):
+                if(nondirected_adjacency_matrix[start][stop]):
+                    graph.add_edge(start, stop)
     return graph
 
-# функция для добавления рёбер
+# initialization directed graph
+def func_initialization_directed_adjacency_list(count_nodes, count_branches):
+    graph = networkx.Graph()
+    for nodes in range(count_nodes):
+        graph.add_node(nodes)
+    directed_adjacency_list= numpy.array([(1),
+                                          (2,4),
+                                          (3,5),
+                                          (0,6),
+                                          (5,7),
+                                          (0,6,8),
+                                          (9),
+                                          (0,8),
+                                          (9),
+                                          (0)])
+    for start in range(len(directed_adjacency_list)):
+        for stop in range(len(directed_adjacency_list[start])):
+            graph.add_edge(start, directed_adjacency_list[start][stop])
+    return graph
+def func_initialization_directed_adjacency_matrix(count_nodes, count_branches):
+    graph = networkx.Graph()
+    for nodes in range(count_nodes):
+        graph.add_node(nodes)
+    directed_adjacency_matrix = numpy.array([(0,1,0,0,0,0,0,0,0,0),
+                                             (0,0,1,0,1,0,0,0,0,0),
+                                             (0,0,0,1,0,1,0,0,0,0),
+                                             (1,0,0,0,0,0,1,0,0,0),
+                                             (0,0,0,0,0,1,0,1,0,0),
+                                             (1,0,0,0,0,0,1,0,1,0),
+                                             (0,0,0,1,0,1,0,0,0,1),
+                                             (1,0,0,0,0,0,0,0,1,0),
+                                             (0,0,0,0,0,0,0,0,0,1),
+                                             (1,0,0,0,0,0,0,0,0,0)])
+    for start in range(len(directed_adjacency_matrix)):
+        for stop in range(len(directed_adjacency_matrix[start])):
+                if(directed_adjacency_matrix[start][stop]):
+                    graph.add_edge(start, stop)
+    return graph
+
+def count_equation(graph):
+    count_equation = numpy.array()
+    graph
+
 def func_add_edge(source, goal, graph=None):
     graph.add_edge(source, goal)
     graph.add_edge(goal, source)
@@ -66,10 +146,30 @@ array_of_EMF = ([[0],
 #I = numpy.linalg.inv(R).dot(E)
 #I = numpy.linalg.solve(array_of_resistance,array_of_EMF)
 
-# создаём экземпляр класса Граф
-graph = func_initialization(COUNT_NODES, COUNT_BRANCHES)
 
-for nodes in range(COUNT_NODES):
-    graph.add_node(nodes)
+graph1 = func_initialization_directed_adjacency_matrix(COUNT_NODES, COUNT_BRANCHES)
+#print(graph1)
 
-print(graph)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# visualization
+
+# edges_trace = go.Scatter(x)
+networkx.draw(graph1)
+
