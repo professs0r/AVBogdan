5635a1b6e89b9e0b0818b064675a4757ca7791a5
professs0r <dok95651@gmail.com>
2022-02-12 00:10:09 +0300

    11.02.22

--- calc_math.py
+++ calc_math.py
@@ -105,106 +105,8 @@ def func_add_edge(source, goal, graph=None):
     graph.add_edge(goal, source)
 
 def func_visualization(graph):
-    X_COORDINATES_NODES = [2,5,25,45,5,25,45,5,25,45]
-    Y_COORDINATES_NODES = [7,5,5,5,25,25,25,45,45,45]
-    pos = networkx.get_node_attributes(graph1,'pos')
-
-    nodes_trace = go.Scatter(x=X_COORDINATES_NODES,
-                             y=Y_COORDINATES_NODES,
-                             hoverinfo='text',
-                             mode='markers',
-                             marker=dict(
-                                 showscale=True,
-                                 colorscale='electric',
-                                 reversescale=True,
-                                 color='tan',
-                                 size=35,
-                                 colorbar=dict(thickness=15,
-                                               title='Node Connection',
-                                               xanchor='left',
-                                               titleside='right')
-                                 ),
-                             line_width=2
-                             )
-    x_edges = []
-    y_edges = []
-
-    for edges in graph.edges():
-        x0, y0 = graph.nodes[edges[0]][pos[edges][0]]
-        x1, y1 = graph.nodes[edges[1]][pos[edges][1]]
-        x_edges.append(x0)
-        x_edges.append(x1)
-        x_edges.append(None)
-        y_edges.append(y0)
-        y_edges.append(y1)
-        y_edges.append(None)
-
-    edges_trace = go.Scatter(x=x_edges,
-                             y=y_edges,
-                             line=dict(width=0.5, color='#888'),
-                             hoverinfo='none',
-                             mode='lines')
-    fig = go.Figure(data=[edges_trace,nodes_trace],
-            layout = go.Layout(
-                title='Graph',
-                titlefont_size=16,
-                showlegend=False,
-                hovermode='closest',
-                margin=dict(b=20, l=5, r=5, t=40),
-                annotations=[dict(text="Ammotations",
-                                  showarrow=False,
-                                  xref="paper",
-                                  yref="paper",
-                                  x=0.005,
-                                  y=-0.0025
-                    )],
-                xaxis=dict(showgrid=False, zeroline=False, showticklables=False),
-                yaxis=dict(showgrid=False, zeroline=False, showticklables=False),
-                ))
-    fig.show()
-
-array_of_resistance = numpy.zeros((COUNT_BRANCHES, COUNT_BRANCHES))
-array_of_EMF = numpy.zeros((COUNT_BRANCHES, 1))
-
-array_of_resistance = ([[0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,1],
-                       [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
-                       [0,-1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,0,0,1,-1,0,0,0,0,0,0,1,0,0,0],
-                       [-1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0],
-                       [0,0,-1,-1,1,0,0,0,1,0,0,0,1,0,0,0,0],
-                       [0,0,0,0,-1,-1,0,0,0,0,1,0,0,0,0,0,0],
-                       [0,0,0,0,0,0,0,-1,0,1,0,0,0,0,1,0,0],
-                       [0,0,0,0,0,0,0,0,-1,-1,0,1,0,0,0,0,0],
-                       [1.35,-1.17,-2.01,1.25,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,2.01,0,2.01,-1.68,-2.55,0,0,0,0,0,0,0,0,0,0],
-                       [0,0,0,-1.25,0,0,0,1.08,-1.25,1.79,0,0,0,0,0,0,0],
-                       [0,0,0,0,-2.01,0,0,0,1.25,0,-2.44,2.01,0,0,0,0,0],
-                       [0,1.17,0,0,0,0,2.55,0,0,0,0,0,0,70.1,0,0,0.1],
-                       [0,0,0,0,0,0,0,0,0,-1.79,0,-2.01,0,0,40,-44.1,0],
-                       [-1.35,0,0,0,0,0,0,-1.08,0,0,0,0,0,0,-40,0,-0.1],
-                       [-1.35,0,0,-1.25,0,0,0,0,0,0,0,0,-40,0,0,0,-0.1]]
-                       )
-
-array_of_EMF = ([[0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [0],
-                 [630],
-                 [0],
-                 [-630],
-                 [-630]])
-
-
-def func_test_visualization():
+    #G = graph
+    #print(G)
     G = nx.random_geometric_graph(10, 0.125)
     
     x_edges = []
@@ -269,13 +171,125 @@ def func_test_visualization():
     fig.show()
 
 
+array_of_resistance = numpy.zeros((COUNT_BRANCHES, COUNT_BRANCHES))
+array_of_EMF = numpy.zeros((COUNT_BRANCHES, 1))
+
+array_of_resistance = ([[0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,1],
+                       [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
+                       [0,-1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,0,0,1,-1,0,0,0,0,0,0,1,0,0,0],
+                       [-1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0],
+                       [0,0,-1,-1,1,0,0,0,1,0,0,0,1,0,0,0,0],
+                       [0,0,0,0,-1,-1,0,0,0,0,1,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0,-1,0,1,0,0,0,0,1,0,0],
+                       [0,0,0,0,0,0,0,0,-1,-1,0,1,0,0,0,0,0],
+                       [1.35,-1.17,-2.01,1.25,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,2.01,0,2.01,-1.68,-2.55,0,0,0,0,0,0,0,0,0,0],
+                       [0,0,0,-1.25,0,0,0,1.08,-1.25,1.79,0,0,0,0,0,0,0],
+                       [0,0,0,0,-2.01,0,0,0,1.25,0,-2.44,2.01,0,0,0,0,0],
+                       [0,1.17,0,0,0,0,2.55,0,0,0,0,0,0,70.1,0,0,0.1],
+                       [0,0,0,0,0,0,0,0,0,-1.79,0,-2.01,0,0,40,-44.1,0],
+                       [-1.35,0,0,0,0,0,0,-1.08,0,0,0,0,0,0,-40,0,-0.1],
+                       [-1.35,0,0,-1.25,0,0,0,0,0,0,0,0,-40,0,0,0,-0.1]]
+                       )
+
+array_of_EMF = ([[0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [0],
+                 [630],
+                 [0],
+                 [-630],
+                 [-630]])
+
+
+def func_test_visualization(graph):
+    G = graph
+    print(G)
+    #G = nx.random_geometric_graph(10, 0.125)
+    X_COORDINATES_NODES = [2,5,25,45,5,25,45,5,25,45]
+    Y_COORDINATES_NODES = [7,5,5,5,25,25,25,45,45,45]
+
+    x_edges = []
+    y_edges = []
+
+    for edges in G.edges():
+        x0 = G.nodes[edges[0]][X_COORDINATES_NODES[edges]]
+        x1 = G.nodes[edges[1]][X_COORDINATES_NODES[edges]]
+        y0 = G.nodes[edges[0]][Y_COORDINATES_NODES[edges]]
+        y1 = G.nodes[edges[1]][Y_COORDINATES_NODES[edges]]
+        x_edges.append(x0)
+        x_edges.append(x1)
+        x_edges.append(None)
+        y_edges.append(y0)
+        y_edges.append(y1)
+        y_edges.append(None)
+    
+    edges_trace = go.Scatter(x=x_edges,
+                             y=y_edges,
+                             line=dict(width=0.5, color='#888'),
+                             hoverinfo='none',
+                             mode='lines')
+    
+    x_nodes = []
+    y_nodes = []
+
+    for node in G.nodes():
+        x,y = G.nodes[node]['pos']
+        x_nodes.append(x)
+        y_nodes.append(y)
+
+    nodes_trace = go.Scatter(x=X_COORDINATES_NODES,
+                             y=Y_COORDINATES_NODES,
+                             hoverinfo='text',
+                             mode='markers',
+                             marker=dict(
+                                 showscale=True,
+                                 colorscale='electric',
+                                 reversescale=True,
+                                 color='tan',
+                                 size=35,
+                                 colorbar=dict(thickness=15,
+                                               title='Node Connection',
+                                               xanchor='left',
+                                               titleside='right')
+                                 ),
+                             line_width=2
+                             )
+    fig = go.Figure(data=[edges_trace, nodes_trace],
+            layout=go.Layout(
+                title='Graph',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[dict(
+                    text="Annotation text",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+    fig.show()
+
+
 # строки кода ниже решают системы уравнений (можно пользоваться любой из них результат одинаковый)
 #I = numpy.linalg.inv(R).dot(E)
 #I = numpy.linalg.solve(array_of_resistance,array_of_EMF)
 
 
-print("graph1 - graph")
 graph1 = func_initialization_directed_adjacency_matrix(COUNT_NODES, COUNT_BRANCHES)
+
 #print(graph1)
 #for edges in graph1.edges():
 #    print(graph1.edges())
@@ -300,11 +314,20 @@ graph1 = func_initialization_directed_adjacency_matrix(COUNT_NODES, COUNT_BRANCH
 
 # visualization
 
+#func_test_visualization(graph1)
+#func_visualization(graph1)
+
 # 2 strings below drawing graph by standart libraries
 #networkx.draw(graph1, with_labels = True)
 #plt.show()
 
-#func_visualization(graph1)
-#networkx.draw(graph1, pos)
-#plt.show()
-func_test_visualization()
+G = nx.random_geometric_graph(10, 0.125)
+print(G.edges())
+print(G.nodes)
+print(graph1.edges())
+print(graph1.nodes)
+# ОСТАНОВИЛСЯ НА ЭТОМ МЕСТЕ!! ПЫТЮСЬ РАЗОБРАТЬСЯ С МАССИВОМ КООРДИНАТ УЗЛОВ
+X_COORDINATES_NODES = [2,5,25,45,5,25,45,5,25,45]
+Y_COORDINATES_NODES = [7,5,5,5,25,25,25,45,45,45]
+#for i in range(len(X_COORDINATES_NODES)):
+#    print(X_COORDINATES_NODES[i])
