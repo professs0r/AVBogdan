a752ed0ad2267c9f9df495835a1a1e7680431eb9
professs0r <dok95651@gmail.com>
2022-09-21 20:44:23 +0300

    full working programm for scheme with 10 nodes and 21 branches: counting all possible spanning trees and calculated in theese trees current

--- just_do_it.py
+++ just_do_it.py
@@ -1,3 +1,5 @@
+import networkx as nx
+
 import test_node_potential_algo as algo
 import numpy as np
 import spannin_tree as st
@@ -9,7 +11,6 @@ import spannin_tree as st
 #                                        START SOURCE DATA
 
 COUNT_NODES = 10
-COUNT_BRANCHES = 17
 
 #edge_0 = (source=0, finish=1, resistance=70.1,
 # voltage=630, type='СИП', length=1000, cross_section=35, I=0,
@@ -18,19 +19,18 @@ COUNT_BRANCHES = 17
 
 edge_0 = (0, 1, 0.1, 630, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_1 = (1, 2, 1.17, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
-edge_2 = (2, 3, 2.55, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
-edge_3 = (1, 4, 1.35, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
+edge_2 = (1, 4, 1.35, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
+edge_3 = (2, 3, 2.55, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_4 = (2, 5, 2.01, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_5 = (3, 6, 1.68, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_6 = (4, 5, 1.25, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
-edge_7 = (5, 6, 2.01, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
-edge_8 = (4, 7, 1.08, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
+edge_7 = (4, 7, 1.08, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
+edge_8 = (5, 6, 2.01, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_9 = (5, 8, 1.25, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_10 = (6, 9, 2.44, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_11 = (7, 8, 1.79, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_12 = (8, 9, 2.01, 0, 0, 1000, 35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 
-edge_nagr_0 = (1, 0, 39.69, 0, 0, -1000, -35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_nagr_1 = (2, 0, 26.46, 0, 0, -1000, -35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_nagr_2 = (3, 0, 172.56, 0, 0, -1000, -35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 edge_nagr_3 = (4, 0, 79.38, 0, 0, -1000, -35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
@@ -47,7 +47,52 @@ edge_nagr_8 = (9, 0, 110.25, 0, 0, -1000, -35, 0, 'Al', 0, 0, 0.89, 0, 0, 0)
 #                                        START WORKING ALGO
 #                                        START WORKING ALGO
 #                                        START WORKING ALGO
-
+edges_lines = np.array([edge_0,
+                        edge_1,
+                        edge_2,
+                        edge_3,
+                        edge_4,
+                        edge_5,
+                        edge_6,
+                        edge_7,
+                        edge_8,
+                        edge_9,
+                        edge_10,
+                        edge_11,
+                        edge_12])
+edges_nagr = np.array([edge_nagr_1,
+                       edge_nagr_2,
+                       edge_nagr_3,
+                       edge_nagr_4,
+                       edge_nagr_5,
+                       edge_nagr_6,
+                       edge_nagr_7,
+                       edge_nagr_8])
+"""
+# для расчёта токов
+edges = np.array([edge_0,
+                  edge_1,
+                  edge_2,
+                  edge_3,
+                  edge_4,
+                  edge_5,
+                  edge_6,
+                  edge_7,
+                  edge_8,
+                  edge_9,
+                  edge_10,
+                  edge_11,
+                  edge_12,
+                  edge_nagr_1,
+                  edge_nagr_2,
+                  edge_nagr_3,
+                  edge_nagr_4,
+                  edge_nagr_5,
+                  edge_nagr_6,
+                  edge_nagr_7,
+                  edge_nagr_8])
+"""
+# для построения всех остовных деревьев
 edges = np.array([edge_0,
                   edge_1,
                   edge_2,
@@ -61,14 +106,9 @@ edges = np.array([edge_0,
                   edge_10,
                   edge_11,
                   edge_12])
+
+
 graph = algo.func_edges_to_undirected_graph(edges, COUNT_NODES)
-print(graph)
-"""
-matrix = algo.func_list_to_matrix(directed_adjacency_list, COUNT_NODES)
-nodes = algo.func_count_of_nodes(matrix)
-branches = algo.func_count_of_branches(directed_adjacency_list)
-graph = algo.func_edges_to_undirected_graph(edges, nodes)
-"""
 
 # подсчёт и перечисление всех остовных деревьев
 trees = st.func_networkx_build_spanning_tree(graph)
@@ -80,13 +120,25 @@ for tree in trees:
         list_edges_in_spanning_tree.append(edge[0])
     # готовый список рёбер остовного дерева
     print(list_edges_in_spanning_tree)
-    print(type(list_edges_in_spanning_tree))
+    new_graph = algo.func_list_of_edges_to_graph(list_edges_in_spanning_tree, COUNT_NODES, edges_lines, edges_nagr)
+    for branch in new_graph.edges():
+        print(branch)
+    algo.func_calculated_current_node_potential_algo(new_graph)
+    iter = 0
+    iter += 1
+    if iter == 190:
+        print("Проверка!")
+    for branch in new_graph.edges():
+        print(branch)
+        print(new_graph.edges[branch]['I'])
+    new_graph.clear()
     list_edges_in_spanning_tree.clear()
 
 """
 # расчёт вспомогательных величин и значений токов в ветвях
-algo.func_calculating_support_variables(graph)
-algo.func_calculated_current_node_potential_algo(graph, nodes, nodes-1, matrix, directed_adjacency_list)
+#algo.func_calculating_support_variables(graph)
+#algo.func_calculated_current_node_potential_algo(graph)
+algo.func_calculated_current_node_potential_algo(graph)
 print("Величины токов в исходном графе: ")
 for branch in graph.edges():
     print(branch)
